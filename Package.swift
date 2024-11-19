d94e988607f442e570a95a1cd0273254a2a046c8
eonist <30n1st@gmail.com>
2024-11-19 05:20:57 +0100

    Downgrade to swift 5.9

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "OnboardKit",
     platforms: [
-      .macOS(.v15),
-      .iOS(.v18)
+      .macOS(.v14),
+      .iOS(.v17)
     ],
     products: [
         .library(
